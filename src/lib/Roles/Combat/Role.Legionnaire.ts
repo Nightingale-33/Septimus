ba873975f99fb00b694ad0356be4d122a9558e83
Toby Lawrance <toby.lawrance@gmail.com>
2026-03-19 21:32:29 +0000

    Sort Legionnaires to let move fail before death

--- src/lib/Roles/Combat/Role.Legionnaire.ts
+++ src/lib/Roles/Combat/Role.Legionnaire.ts
@@ -3,12 +3,18 @@ import { GetCreepMemory, GetLargestBody, SpawnCreep } from "../../../utils/Spawn
 
 export const LEGIONNAIRE = "Legionnaire"
 
+export const combatBodySortOrder : BodyPartConstant[] = [TOUGH,CARRY,MOVE,WORK,ATTACK,RANGED_ATTACK,CLAIM,HEAL];
+
+function SortCombatParts(bp : BodyPartConstant) : number {
+  return combatBodySortOrder.indexOf(bp);
+}
+
 export function SpawnLegionnaire(spawn: StructureSpawn, province: Province) : string | null
 {
   const baseBody = [TOUGH, ATTACK, MOVE];
   const bodyAddon = [TOUGH,ATTACK];
 
-  let largestBody = GetLargestBody(spawn, baseBody, bodyAddon,4);
+  let largestBody = GetLargestBody(spawn, baseBody, bodyAddon,4,true,true,SortCombatParts);
   if (largestBody.length == 0) {
     return null;
   }
